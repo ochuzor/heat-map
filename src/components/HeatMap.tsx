517ba3a21786fa3c7a9e9e73783655db4230ab7c
Chinedu Eze <ochuzor@gmail.com>
2021-08-16 19:46:33 +0300

    refactored out utils functions

--- src/components/HeatMap.tsx
+++ src/components/HeatMap.tsx
@@ -3,48 +3,7 @@ import { groupBy, map, max, min, range, filter, reduce, add, flatten } from "lod
 
 import type { DataItem, HeatMapItem } from "../Types";
 import { HeatMapRow } from "./HeatMapRow";
-
-const NUM_CATEGORIES = 4;
-
-const getStepValue = (nums: number[], categoryCount = NUM_CATEGORIES) => {
-    const maxValue = max(nums) || 0;
-    const minValue = min(nums) || 0;
-    const range = maxValue - minValue;
-
-    if (range <= 0) return 1;
-
-    const step = Math.ceil(range / categoryCount);
-
-    return step;
-};
-
-const getCategory = (val: number, step: number, numberOfCategories = NUM_CATEGORIES) => {
-    if (numberOfCategories < 2) {
-        throw new Error("Must have at least 2 categories");
-    }
-
-    if (val === 0) return 0;
-    if (numberOfCategories === 2) return 1;
-
-    for (let i = 1; i < numberOfCategories; i++) {
-        if (val <= (step * i)) return i;
-    }
-
-    return numberOfCategories;
-};
-
-// maps indices to tile color
-const CATEGORY_COLOR_MAP = [
-    "#f7f9f9",
-    "#DDDAB6",
-    "#9FB089",
-    "#C77E6B",
-    "#3E3732",
-];
-
-const getColor = (category: number, defaultColour: string = "black") => {
-    return CATEGORY_COLOR_MAP[category] || defaultColour;
-};
+import { getColor, getCategory, getStepValue } from "./utils";
 
 export type HeatMapProps = {
     data: DataItem[];
---
+++ src/components/utils.ts
@@ -0,0 +1,43 @@
+import { max, min } from "lodash";
+
+const NUM_CATEGORIES = 4;
+
+export const getStepValue = (nums: number[], categoryCount = NUM_CATEGORIES) => {
+    const maxValue = max(nums) || 0;
+    const minValue = min(nums) || 0;
+    const range = maxValue - minValue;
+
+    if (range <= 0) return 1;
+
+    const step = Math.ceil(range / categoryCount);
+
+    return step;
+};
+
+export const getCategory = (val: number, step: number, numberOfCategories = NUM_CATEGORIES) => {
+    if (numberOfCategories < 2) {
+        throw new Error("Must have at least 2 categories");
+    }
+
+    if (val === 0) return 0;
+    if (numberOfCategories === 2) return 1;
+
+    for (let i = 1; i < numberOfCategories; i++) {
+        if (val <= (step * i)) return i;
+    }
+
+    return numberOfCategories;
+};
+
+// maps indices to tile color
+const CATEGORY_COLOR_MAP = [
+    "#f7f9f9",
+    "#DDDAB6",
+    "#9FB089",
+    "#C77E6B",
+    "#3E3732",
+];
+
+export const getColor = (category: number, defaultColour: string = "black") => {
+    return CATEGORY_COLOR_MAP[category] || defaultColour;
+};
